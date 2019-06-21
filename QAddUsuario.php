e1757cf57708aa0eef070e136a5ca4f87dca61d8
GustavoMariano <47873305+GustavoMariano@users.noreply.github.com>
2019-06-21 03:29:13 -0300

    Issue #1 Erro cadastro novo usuario
    Corrigido o erro de cadastrar novo usuario e pegar as permissões de admin, mesmo selecionando outra opção.

--- QAddUsuario.php
+++ QAddUsuario.php
@@ -6,7 +6,7 @@ $add_usuario = array(
 	':nome' => $_POST['nome'],
 	':usuario' => $_POST['usuario'],
 	':senha' => $_POST['senha'],
-	':nivel' => $_SESSION['nivel']
+	':nivel' => $_POST['nivel']
 );
 
 $sql = $pdo->prepare("INSERT INTO `tb_usuario`(`login`, `senha`, `nome`, `nivel`) VALUES (:usuario,:senha,:nome,:nivel)");
